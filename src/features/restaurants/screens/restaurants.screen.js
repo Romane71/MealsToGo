4fe50217adab6687577846593bb1858162527f00
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-06-25 23:46:25 +0100

    hooking up context

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -23,20 +23,23 @@ const RestaurantList = styled(FlatList).attrs({
 `;
 
 export const RestaurantsScreen = () => {
-const restaurantContext = useContext(RestaurantsContext);
-console.log(restaurantContext)
+const { isLoading, error, restaurants} = useContext(RestaurantsContext);
+console.log(error)
 return (
  <SafeArea >
   <SearchContainer>
         <Searchbar />
        </SearchContainer>
   <RestaurantList 
-  data={restaurantContext.restaurants}
-  renderItem={()  => (
+  data={restaurants}
+  renderItem={({ item })  => {
+    console.log(item)
+    return ( 
     <Spacer position="bottom" size="large">
-   <RestaurantInfoCard  />
+   <RestaurantInfoCard  restaurant={item}/>
    </Spacer>
-  )}
+    );
+  }}
   keyExtractor={(item) => item.name}
   />
   </SafeArea>
--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -4,10 +4,35 @@ import { restaurantsRequest, restaurantsTransform } from './restaurants.service'
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({children}) => {
+    const [restaurants, setRestaurants] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+const retrieveRestaurants =  () => {
+    setIsLoading(false);
+    setTimeout(() => {
+    restaurantsRequest().then(restaurantsTransform).then((restaurants) => {
+        setRestaurants(restaurants);
+}).catch((error) => {
+    setIsLoading(false);
+    setError(error);
+});
+    }, 2000)
+};
+    useEffect(() => {
+
+retrieveRestaurants();
+
+    }, []);
+
+    console.log(restaurants)
+
     return (
         <RestaurantsContext.Provider
              value={{
-            restaurants: [1, 2,3 ]
+            restaurants,
+            isLoading,
+            error
         }}>
             {children}
         </RestaurantsContext.Provider>
