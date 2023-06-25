6c115a99a34538af62daffe0b245da15d55f10fe
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-06-25 02:56:47 +0100

    Adding mock data

--- src/features/restaurants/components/restaurant-info-card.styles.js
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import React from 'react';
 import { Card } from "react-native-paper";
 import { Text } from "../../../components/typography/text.component";
 import { View } from "react-native";
