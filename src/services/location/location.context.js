1e46a01d66fee04d15f09bb55a4d1fb75fa0cb89
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-06-26 02:45:47 +0100

    Hooking up search bar

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -5,30 +5,30 @@ import { locationRequest, locationTransform} from './location.service';
 export const LocationContext = createContext();
 
 export const LocationContextProvider = ({children}) => {
-const [keyword, setKeyword] = useState("antwerp");
+const [keyword, setKeyword] = useState("San Fransisco");
 const [location, setLocation ] = useState(null);
 const [isLoading, setIsLoading] = useState(false);
 const [error, setError] = useState(null);
 
 const onSearch = (searchKeyword) => { 
-    console.log(searchKeyword);
     setIsLoading(true);
     setKeyword(searchKeyword);
+ if(!searchKeyword.length) {
+    // do not do anything
+    return;
+ }
     locationRequest(searchKeyword.toLowerCase())
     .then(locationTransform)
     .then(result => {
         setIsLoading(false);
         setLocation(result);
-        console.log(result)
     }).catch(error => {
        setIsLoading(false);
        setError(error);
     })
 }
 
-useEffect(() => {
-    onSearch(keyword)
-}, []);
+
 
     return (
         <LocationContext.Provider
