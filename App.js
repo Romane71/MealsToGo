6b3fa6cd5753d39b1f7517d848470633eec816b3
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-06-26 01:59:18 +0100

    Geocoding context and set up

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
+import { LocationContextProvider } from "./src/services/location/location.context";
 
 const Tab = createBottomTabNavigator();
 
@@ -42,6 +43,7 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <LocationContextProvider>
         <RestaurantsContextProvider>
         <NavigationContainer>
           <Tab.Navigator
@@ -57,6 +59,7 @@ export default function App() {
           </Tab.Navigator>
         </NavigationContainer>
         </RestaurantsContextProvider>
+        </LocationContextProvider>
       </ThemeProvider>
 
       <ExpoStatusBar style="auto" />
---
+++ src/services/location/location.context.js
@@ -0,0 +1,43 @@
+import React, {createContext, useState, useEffect} from 'react';
+import { locationRequest, locationTransform} from './location.service';
+
+
+export const LocationContext = createContext();
+
+export const LocationContextProvider = ({children}) => {
+const [keyword, setKeyword] = useState("antwerp");
+const [location, setLocation ] = useState(null);
+const [isLoading, setIsLoading] = useState(false);
+const [error, setError] = useState(null);
+
+const onSearch = (searchKeyword) => { 
+    console.log(searchKeyword);
+    setIsLoading(true);
+    setKeyword(searchKeyword);
+    locationRequest(searchKeyword.toLowerCase())
+    .then(locationTransform)
+    .then(result => {
+        setIsLoading(false);
+        setLocation(result);
+        console.log(result)
+    }).catch(error => {
+       setIsLoading(false);
+       setError(error);
+    })
+}
+
+useEffect(() => {
+    onSearch(keyword)
+}, []);
+
+    return (
+        <LocationContext.Provider
+        value={{
+            isLoading,
+            error,
+            location,
+            search: onSearch,
+            keyword,
+        }}>{children}</LocationContext.Provider>
+    );
+};
