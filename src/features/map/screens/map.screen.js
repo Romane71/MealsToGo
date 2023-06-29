6fd695c44f251e5c65640e677231224bc2e700a5
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-06-29 01:34:04 +0100

    custom map callout

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,54 +1,58 @@
-
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
+
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+
 import { Search } from "../components/search.component";
+import { MapCallout } from "../components/map-callout.component";
 
-
-
-const Map = styled(MapView)`
-height: 100%;
-width: 100%
+export const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
 `;
+
 export const MapScreen = () => {
-    const { location } = useContext(LocationContext);
-    const { restaurants = [] } = useContext(RestaurantsContext);
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
 
-    const [latDelta, setLatDelta ] = useState(0);
+  const [latDelta, setLatDelta] = useState(0);
 
-    const { viewport, lat, lng } = location;
-    console.log(viewport.northeast)
+  const { lat, lng, viewport } = location;
 
-    useEffect(() => {
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
 
-        const northeastLat = viewport.northeast.lat;
-        const southwestLat = viewport.southwest.lat;
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
 
-  
-        setLatDelta(northeastLat - southwestLat);
-    }, [location, viewport])
-     
-     return (
-     <>
-     <Search />
-    <Map
-    region={{
-        latitude: lat,
-        longitude: lng,
-        latitudeDelta: latDelta,
-        longitudeDelta: 0.02,
-    }}
-    
-    >
-   {restaurants.map(() => {
-
-    return null
-  
-   })}
-    </Map>
+  return (
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return (
+              <MapView
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            >
+        </MapView>
+          );
+        })}
+      </Map>
     </>
-);
+  );
 };
-
